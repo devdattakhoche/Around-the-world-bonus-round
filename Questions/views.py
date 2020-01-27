6ec56a91ff155688e594f5e49ca26b9f98350a1b
KhadapkarS20 <shreyaskhadapkar@gmail.com>
2020-01-28 00:00:20 +0530

    Random Function

--- Questions/views.py
+++ Questions/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 # Create your views here.
 def Home(request):
-    return render(request,'ATW/Home.html')
+    question_no = random.randrange(1,81)
+    # dictionary to be passed
+    x = {}
+    x['mytext'] = question_no
+ 
+    return render(request,'ATW/Home.html',x)
 
 def Questions(request,id):
     print(id)
